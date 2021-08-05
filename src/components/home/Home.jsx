ce777444e16c43e648c6e6290639962086622aaa
nimos <nimos.cmp@gmail.com>
2021-08-05 01:14:31 -0500

    Set main structure to home and add it to app

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,15 @@
 const Home = () => {
     return (
     <>
-            
+        <section>
+            <div>
+                <h1>Title</h1>
+                <button>Action</button>
+                <img src="" alt="" />
+                <div className="trapezoid"></div>
+                <div className="figure"></div>
+            </div>
+        </section>        
     </>
     )
 }
